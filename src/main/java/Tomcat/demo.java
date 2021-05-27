abe360e1b7620e9bd0af72c36e917fd9a5019bb4
Quocanh <phiquocanh717@gmail.com>
2021-05-27 15:26:15 +0700

    'test'

---
+++ src/main/java/Tomcat/demo.java
@@ -0,0 +1,24 @@
+package Tomcat;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "demo", urlPatterns = "/index")
+public class demo extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<h1>Hello world</h1>");
+        writer.println("</html>");
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
